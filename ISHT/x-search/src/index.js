e5c70217ec5442f86a5271e67090c1392f6f3e3e
melhuet <melanie.huet@bonitasoft.com>
2019-03-01 16:40:27 +0100

    Move folders in ISHT folder

--- ISHT/x-search/src/index.js
+++ ISHT/x-search/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jQuery from 'jquery'; //var jquery = require('jquery')
 
 class XSearch extends HTMLElement {
   connectedCallback() {
@@ -11,10 +10,6 @@ class XSearch extends HTMLElement {
     const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
 
     ReactDOM.render(<a href={url}>{name}</a>, mountPoint);
-    jQuery(mountPoint).append("<div>xsearch</div>")
-
   }
 }
 customElements.define('x-search', XSearch);
-
-export default XSearch;
